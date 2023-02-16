9f911af37d3e2c79862509f7e61cd0b4acba9202
vagaresnehal <vagaresnehal@gmail.com>
2023-02-16 17:03:17 +0530

    update file
    update file

--- 01_variable_assign.js
+++ 01_variable_assign.js
@@ -1,19 +1,22 @@
-console.log("******************step1********************");
+console.log("------------------------Before Initialization-------------------------");
 var myFullName;//variable declaration
 console.log(myFullName,".....variable declaration......");
 myFullName="Snehal Vagare";//variable initialization
 console.log("my full name is:",myFullName,".....variable initialization......");
+console.log("-------------------------Updated Full Name---------------------------------");
 myFullName="Snehal Appasaheb Vagare";
 console.log("my full name is:",myFullName,"......variable update....");
-console.log("*******************step2**********************");
+console.log("--------------------------After Initialization-------------------------------");
 var myLove="I love only JS";
 console.log(myLove,"......variable declaration and initialization......");
-console.log("*********************step3***********************");
-var firstName="Snehal";
-var lastName="Vagare";
-var age="25";
-var address="Mahud";
-var pincode="413306";
-var gender="Female";
-var isMarried="yes";
+console.log("-------------------------------------Personal Details-------------------------------------------");
+console.log("-------------------------------------------------------------------------------------------------------------------------------");
+var firstName="Snehal||";
+var lastName="Vagare||";
+var age="25||";
+var address="Mahud||";
+var pincode="413306||";
+var gender="Female||";
+var isMarried="yes||";
 console.log("first name is:",firstName,"last name is:",lastName,"Age:",age,"Address:",address,"Pincode:",pincode,"Gender:",gender,"Martial Status:",isMarried);
+console.log("-----------------------------------------------------------------------------------------------------------------------------------------");
