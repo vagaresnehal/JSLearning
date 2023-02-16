61892ac3044b4d0e9a321e439e670053e837711f
vagaresnehal <vagaresnehal@gmail.com>
2023-02-16 16:25:36 +0530

    updated file
    updated file

---
+++ 01_variable_assign.js
@@ -0,0 +1,19 @@
+console.log("******************step1********************");
+var myFullName;//variable declaration
+console.log(myFullName,".....variable declaration......");
+myFullName="Snehal Vagare";//variable initialization
+console.log("my full name is:",myFullName,".....variable initialization......");
+myFullName="Snehal Appasaheb Vagare";
+console.log("my full name is:",myFullName,"......variable update....");
+console.log("*******************step2**********************");
+var myLove="I love only JS";
+console.log(myLove,"......variable declaration and initialization......");
+console.log("*********************step3***********************");
+var firstName="Snehal";
+var lastName="Vagare";
+var age="25";
+var address="Mahud";
+var pincode="413306";
+var gender="Female";
+var isMarried="yes";
+console.log("first name is:",firstName,"last name is:",lastName,"Age:",age,"Address:",address,"Pincode:",pincode,"Gender:",gender,"Martial Status:",isMarried);
